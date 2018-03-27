3dc4c0f575d5383e02e7fa2bcde492a7a6aa2b10
Chrissrey <calmaraz@mail.csuchico.edu>
2018-03-27 11:49:50 -0700

    X&O Game

--- main.cpp
+++ main.cpp
@@ -64,7 +64,8 @@ int main()
     for(int r=0; r<ROWS;r++)
     {
         for(int c=0; c<COLUMNS;c++)
-        cout<<board[r][c]<<" "<<endl;
+        cout<<board[r][c]<<" ";
+        cout<<endl;
 
 
 
